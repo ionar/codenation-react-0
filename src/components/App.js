982ea13fb6505b7b89e0fbab51b96461aeb11dab
Ionar Rafael <irm@ionar.com.br>
2019-07-24 07:35:46 -0300

    tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,42 @@
 import React, { Component } from 'react';
-import Navbar from './Navbar'
-import RecipeItem from './RecipeItem'
-import recipes from '../sample_data/recipes.json'
+import Navbar from './Navbar';
+import RecipeItem from './RecipeItem';
+import recipes from '../sample_data/recipes.json';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.recipes = recipes.results;
-    this.state = {
-      searchString: ''
-    };
-  }
+  recipes = recipes.results;
+  state = {
+    searchString: '',
+    recipesFiltered: this.recipes
+  };
   
   onChangeInput = (e) => {
     this.setState({
       searchString: e.target.value
     })
+    const listTemp = this.filtro(this.state.searchString);
+    this.setState({
+      recipesFiltered: listTemp
+    })
+    console.log(this.state.searchString);
+  }
+
+  filtro = (termo) => {
+    return this.recipes.filter( recipe => 
+      recipe.title.toLowerCase().indexOf(termo.toLowerCase()) || recipe.ingredients.toLowerCase().indexOf(termo.toLowerCase())
+    )
   }
 
   render() { 
+
     return (
       <div className="App">
         <Navbar value={this.state.searchString} onChange={this.onChangeInput} />
         <div className="container mt-10">
           <div className="row">
-            {this.recipes.map(recipe =>(
+            {this.state.recipesFiltered.map( (recipe, index) =>(
               <RecipeItem 
+                key={index}
                 title={recipe.title}
                 href={recipe.href}
                 ingredients={recipe.ingredients}
